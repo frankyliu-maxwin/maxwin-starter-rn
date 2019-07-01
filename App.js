bc5322ecd123a6f98c2bb871f6cfd38527d07a3d
郭哲倫 <gameking93@gmail.com>
2019-07-01 13:37:54 +0800

    chore(Init): 建立環境

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Button } from 'react-native-paper';
+
+export default class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+        Press_Count: 0,
+    };
+  }
+
+  _renderButton = () => {
+    return <Button mode="contained" onPress={()=>{ this.setState({ Press_Count: this.state.Press_Count + 1 }) }}>Press</Button>
+  }
+  render(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'space-between', flexDirection: 'column' }}>
+          <StatusBar hidden/>
+          <View style={styles.top}>
+            {this._renderButton()}
+            {this._renderButton()}
+          </View>
+          <View style={styles.center}>
+            <Text style={{ fontSize: 30 }}>{`Touch Count: ${this.state.Press_Count}`}</Text>
+            {this._renderButton()}
+          </View>
+          <View style={styles.bottom}>
+            {this._renderButton()}
+            {this._renderButton()}
+          </View>
+        </View>
+      );
+    }
+}
+
+const styles = StyleSheet.create({
+  top: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  center: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  bottom: {
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
